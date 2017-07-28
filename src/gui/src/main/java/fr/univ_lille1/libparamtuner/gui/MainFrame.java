2d401c77e5d1d93afd78fef47582fc70d3ba108f
Géry CASIEZ <gery.casiez@univ-lille1.fr>
2017-07-28 15:23:12 +0200

    new interface for ParamTunerGUI

--- src/gui/src/main/java/fr/univ_lille1/libparamtuner/gui/MainFrame.java
+++ src/gui/src/main/java/fr/univ_lille1/libparamtuner/gui/MainFrame.java
@@ -1,6 +1,6 @@
 /*
  *  libParamTuner
- *  Copyright (C) 2017 Marc Baloup, Veïs Oudjail
+ *  Copyright (C) 2017 Marc Baloup, Veïs Oudjail, Géry Casiez
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -18,7 +18,12 @@
 package fr.univ_lille1.libparamtuner.gui;
 
 import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Vector;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.prefs.Preferences;
 
 import fr.univ_lille1.libparamtuner.gui.parameters_panel.ParameterPanel;
 import fr.univ_lille1.libparamtuner.parameters.Parameter;
@@ -26,26 +31,31 @@ import fr.univ_lille1.libparamtuner.parameters.ParameterFile;
 import javafx.application.Application;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
-import javafx.geometry.Pos;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.CheckMenuItem;
 import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.ScrollPane.ScrollBarPolicy;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleButton;
+import javafx.scene.control.SeparatorMenuItem;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.BorderStroke;
 import javafx.scene.layout.BorderWidths;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
-import java.util.prefs.Preferences;
 
 public class MainFrame extends Application {
 	
@@ -54,13 +64,14 @@ public class MainFrame extends Application {
 	
 	final FileChooser fileChooser = new FileChooser();
 	private VBox contentPanel;
-	private TextField textField;
 	public ScrollPane contentScroll; // TODO private
-	private Button btnSave;
+	private MenuItem btnSave;
+	Menu openrecent;
 
 	public DoubleProperty minLabelSize = new SimpleDoubleProperty(0); // TODO private
 	
-	
+	private String filepath = "";
+	private String currentFileName = "";
 	private ParameterFile loadedFile = null;
 	private boolean saved = true;
 	private boolean autosave = true;
@@ -68,7 +79,6 @@ public class MainFrame extends Application {
 	private SaveThread saveThread = new SaveThread();
 
 	private Preferences prefs;
-	
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
@@ -85,17 +95,91 @@ public class MainFrame extends Application {
 			}
 		});
 
+		// Save settings in a persistent way
 		prefs = Preferences.userRoot().node(this.getClass().getName());
+		filepath = prefs.get("lastpath","");
+		if (filepath != "") {
+			getCurrentFilename(filepath);
+			stage.setTitle(currentFileName);
+		}
+		autosave = prefs.getBoolean("autosave", true);
 		
 		BorderPane globalPanel = new BorderPane();
-		
-		scene = new Scene(globalPanel, 500, 600);
-		stage.setScene(scene);
-		stage.sizeToScene();
-		stage.centerOnScreen();
-		
 		globalPanel.setBorder(new Border(new BorderStroke(Color.TRANSPARENT, null, null, new BorderWidths(3))));
 		
+		double windowWidth = prefs.getDouble(getCode(filepath) + "width", 50);
+		double windowHeight = prefs.getDouble(getCode(filepath) + "height", 300);
+		
+		scene = new Scene(globalPanel, windowWidth, windowHeight);
+		stage.setScene(scene);
+		stage.sizeToScene();
+		stage.centerOnScreen();	
+
+		
+		MenuBar menuBar = new MenuBar();
+		menuBar.setUseSystemMenuBar(true);
+		
+		// File
+		Menu menu1 = new Menu("File");
+		KeyCombination krfile = new KeyCodeCombination(KeyCode.F, KeyCombination.ALT_DOWN);
+		menu1.setAccelerator(krfile);
+		
+		// Open...
+		MenuItem open = new MenuItem("Open...");
+		KeyCombination kropen = new KeyCodeCombination(KeyCode.O, KeyCombination.META_DOWN);
+		open.setAccelerator(kropen);
+		open.setOnAction(e -> {
+			File f = fileChooser.showOpenDialog(stage);
+			if (f != null) {
+				filepath = f.getPath();
+				setFilePathAndLoad(filepath);
+				getCurrentFilename(filepath);
+				stage.setTitle(currentFileName);
+			}
+			}
+		);
+		
+		// Open recent
+		//openrecent = new Menu("Open Recent");
+		SeparatorMenuItem smi = new SeparatorMenuItem();
+		
+		// Save
+		btnSave = new MenuItem("Save");
+		KeyCombination krsave = new KeyCodeCombination(KeyCode.S, KeyCombination.META_DOWN);
+		btnSave.setAccelerator(krsave);		
+		btnSave.setOnAction(e -> saveFile());
+		btnSave.setDisable(true);
+		
+		// Auto save
+		CheckMenuItem chckbxAutosave = new CheckMenuItem("Auto save");
+		chckbxAutosave.setSelected(autosave);
+		chckbxAutosave.setOnAction(e -> setAutosave(chckbxAutosave.isSelected()));
+		SeparatorMenuItem smi2 = new SeparatorMenuItem();
+		
+		// Revert file
+		MenuItem btnRevert = new MenuItem("Revert file");
+		KeyCombination krrevert = new KeyCodeCombination(KeyCode.R, KeyCombination.META_DOWN);
+		btnRevert.setAccelerator(krrevert);		
+		btnRevert.setOnAction(e -> loadFile(filepath));
+		
+		menu1.getItems().addAll(open, smi, btnSave, chckbxAutosave, smi2, btnRevert);
+		
+		// Help
+		Menu menu2 = new Menu("Help");
+		
+		// About
+		MenuItem btnAbout = new MenuItem("About");
+		btnAbout.setOnAction(e -> {
+			Alert alert = new Alert(AlertType.INFORMATION);
+			alert.setTitle("About");
+			alert.setHeaderText(null);
+			alert.setContentText("ParamTunerGUI version 1.0\n\nParamTunerGUI is part of libParamTuner\nGet more information on https://github.com/casiez/libparamtuner");
+			
+			alert.showAndWait();
+		});
+		menu2.getItems().add(btnAbout);
+		menuBar.getMenus().addAll(menu1, menu2);
+		globalPanel.setTop(menuBar);
 		
 		fileChooser.setInitialDirectory(new File("."));
 		ExtensionFilter xmlFilter = new ExtensionFilter("XML Files", "*.xml");
@@ -103,65 +187,53 @@ public class MainFrame extends Application {
 		fileChooser.getExtensionFilters().add(new ExtensionFilter("All files", "*"));
 		fileChooser.setSelectedExtensionFilter(xmlFilter);
 		
-		HBox topPanel = new HBox(3);
-		topPanel.setBorder(new Border(new BorderStroke(Color.TRANSPARENT, null, null, new BorderWidths(2, 2, 3, 2))));
-		globalPanel.setTop(topPanel);
-		topPanel.setAlignment(Pos.CENTER);
-		
-		Label lblNewLabel = new Label("XML File :");
-		topPanel.getChildren().add(lblNewLabel);
-		
-		String lastPath = prefs.get("lastpath","");
-		textField = new TextField(lastPath);
-		topPanel.getChildren().add(textField);
-		textField.setPrefWidth(20);
-		HBox.setHgrow(textField, Priority.ALWAYS);
-		textField.setOnAction(e -> loadFile(textField.getText()));
-
-		Button btnExplorer = new Button("...");
-		topPanel.getChildren().add(btnExplorer);
-		btnExplorer.setOnAction(e -> {
-			File f = fileChooser.showOpenDialog(stage);
-			if (f != null)
-				setFilePathAndLoad(f.getPath());
-		});
-		
-		Button btnCharger = new Button("Load");
-		topPanel.getChildren().add(btnCharger);
-		btnCharger.setOnAction(e -> loadFile(textField.getText()));
-		
-		btnSave = new Button("Save");
-		btnSave.setOnAction(e -> saveFile());
-		btnSave.setDisable(true);
-		topPanel.getChildren().add(btnSave);
-		
-		ToggleButton chckbxAutosave = new ToggleButton("Autosave");
-		chckbxAutosave.setSelected(autosave);
-		chckbxAutosave.setOnAction(e -> setAutosave(chckbxAutosave.isSelected()));
-		topPanel.getChildren().add(chckbxAutosave);
 		
 		contentScroll = new ScrollPane();
-		contentScroll.setVbarPolicy(ScrollBarPolicy.ALWAYS);
-		contentScroll.setHbarPolicy(ScrollBarPolicy.AS_NEEDED);
+		contentScroll.setVbarPolicy(ScrollBarPolicy.AS_NEEDED);
+		//contentScroll.setHbarPolicy(ScrollBarPolicy.AS_NEEDED);
 		globalPanel.setCenter(contentScroll);
 		
 		contentPanel = new VBox();
 		contentPanel.setBorder(new Border(new BorderStroke(Color.TRANSPARENT, null, null, new BorderWidths(3))));
 		contentScroll.setContent(contentPanel);
 		contentScroll.setFitToWidth(true);
+
+		
+		scene.widthProperty().addListener(new ChangeListener<Number>() {
+		    @Override public void changed(ObservableValue<? extends Number> observableValue, Number oldSceneWidth, Number newSceneWidth) {
+		        prefs.putDouble(getCode(filepath) + "width", (double)newSceneWidth);
+		    }
+		});
+		
+		scene.heightProperty().addListener(new ChangeListener<Number>() {
+		    @Override public void changed(ObservableValue<? extends Number> observableValue, Number oldSceneHeight, Number newSceneHeight) {
+		        prefs.putDouble(getCode(filepath) + "height", (double)newSceneHeight);
+		    }
+		});
 		
 		saveThread.start();
 		
-		stage.show();
+		if (filepath != "")
+			loadFile(filepath);
 		
-		if (lastPath != "")
-			loadFile(lastPath);
+		stage.show();
 	}
 	
-	
-	
-	
-	
+	private String getCode(String filepath) {
+		String res = "";
+		try {
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			byte bytes[] = md.digest(filepath.getBytes("ISO-8859-1"));
+			res = new String(bytes, "ISO-8859-1");
+			res = res.substring(0, 10); 
+			//System.out.println(res);
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		return res;
+	}
 	
 	private void saveFile() {
 		if (saved || loadedFile == null)
@@ -171,6 +243,11 @@ public class MainFrame extends Application {
 		
 		setSaved(true);
 		
+	}
+	
+	private void getCurrentFilename(String filepath) {
+		String[] parts = filepath.split("/");
+		currentFileName = parts[parts.length-1];
 	}
 	
 	
@@ -240,7 +317,6 @@ public class MainFrame extends Application {
 		
 	}
 	
-	
 	private void clearConfigEntries() {
 		contentPanel.getChildren().forEach(entry -> ((Label)((ParameterPanel)entry).getChildren().get(0)).minWidthProperty().unbind());
 		contentPanel.getChildren().clear();
@@ -251,18 +327,20 @@ public class MainFrame extends Application {
 		contentPanel.getChildren().add(ParameterPanel.fromParameter(this, contentPanel.getChildren().size(), p));
 	}
 	
-	
 	public void setFilePathAndLoad(String path) {
-		textField.setText(path);
+		filepath = path;
+		getCurrentFilename(filepath);
+		stage.setTitle(currentFileName);
 		loadFile(path);
 	}
 	
-	
-	
 	public void onContentModify() {
 		setSaved(false);
-		if (autosave)
+		stage.setTitle(currentFileName + "*");
+		if (autosave) {
 			saveFile();
+			stage.setTitle(currentFileName);
+		}
 	}
 	
 	private void setSaved(boolean s) {
@@ -270,29 +348,24 @@ public class MainFrame extends Application {
 		updateSaveButton();
 	}
 	
-	
 	private void setAutosave(boolean as) {
 		autosave = as;
+		prefs.putBoolean("autosave", as);
 		updateSaveButton();
 		if (autosave && !saved) {
 			saveFile();
 		}
 	}
-	
-	
+		
 	private void updateSaveButton() {
 		btnSave.setDisable(autosave || saved);
+		stage.setTitle(currentFileName);
 	}
-	
-	
-	
-	
 	
 	private class SaveThread extends Thread {
 		
 		private AtomicBoolean wantToSave = new AtomicBoolean(false);
 		
-		@Override
 		public void run() {
 			try {
 				for(;;) {
@@ -313,13 +386,8 @@ public class MainFrame extends Application {
 			}
 		}
 		
-		
-		
 		public void askForSave() {
 			wantToSave.set(true);
-		}
-		
-		
+		}	
 	}
-	
 }
